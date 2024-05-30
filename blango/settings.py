4f09980a38a2d4bfc6008033dc953c55baa6487b
Pedro Melgarejo <pedro@checkwiz.net>
2024-05-30 18:48:49 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +79,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'blango.wsgi.application'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+
 
 
 # Database
