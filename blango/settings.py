7d211167760ff968ce0fe4a4d460c2efba5c331e
Pedro Melgarejo <pedro@checkwiz.net>
2024-07-10 17:34:17 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -72,6 +72,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg',
 
     ]
 
@@ -158,6 +159,13 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
           "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
 
