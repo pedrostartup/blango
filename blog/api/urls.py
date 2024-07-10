d426e7360a9d4bb0f144edfc8fdc836c79139dd0
Pedro Melgarejo <pedro@checkwiz.net>
2024-07-10 21:11:05 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 
 urlpatterns = [
-  path("posts/", PostList.as_view(), name="api_post_list"),
-  path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+  #path("posts/", PostList.as_view(), name="api_post_list"),
+  #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
   path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -25,6 +26,10 @@ schema_view = get_schema_view(
     public=True,
 )
 
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
@@ -38,4 +43,5 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
